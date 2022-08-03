7224a58b986cbbc60041054919fb8190d87d3c3f
ToddDevRepo <todd@LAPTOP-AJE60IS6.localdomain>
2022-08-03 19:32:26 +0100

    Add new passing test to make sure articles model can handle decrements too.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -108,6 +108,25 @@ describe(Endpoints.ARTICLES_END, () => {
       };
       expect(body).toEqual({ updatedArticle: expected });
     });
+    test("returns with status 200 and an article with decremented votes", async () => {
+      const incBy = -5;
+
+      const { body } = await request(app)
+        .patch(`${Endpoints.ARTICLES_END}/2`)
+        .send({ inc_votes: incBy })
+        .expect(200);
+
+      const expected = {
+        article_id: 2,
+        title: "Sony Vaio; or, The Laptop",
+        topic: "mitch",
+        author: "icellusedkars",
+        body: "Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.",
+        created_at: "2020-10-16T05:03:00.000Z",
+        votes: -5,
+      };
+      expect(body).toEqual({ updatedArticle: expected });
+    });
     test("returns with status 422 (unprocessable entity) when there is no send body", async () => {
       const { body } = await request(app)
         .patch(`${Endpoints.ARTICLES_END}/2`)
