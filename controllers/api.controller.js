74d7882c984d418f77098a3d3f4ce06262b00763
ToddDevRepo <todd@LAPTOP-AJE60IS6.localdomain>
2022-08-06 17:12:26 +0100

    Refactor - remove all console logs.

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -2,13 +2,10 @@ const { json } = require("express");
 const { readEndpoints } = require("../models/api.model");
 
 module.exports.getEndpoints = async (request, response, next) => {
-  console.log("Endpoints controller");
   try {
     const json = await readEndpoints();
-    console.log(json);
     response.send(json);
   } catch (error) {
-    console.log(error);
     next(error);
   }
 };
