92909720c49c7c55cb0e021b40600f20f3be70f9
ToddDevRepo <todd@LAPTOP-AJE60IS6.localdomain>
2022-08-07 17:15:53 +0100

    Fix - prevent attempt to require node env files if in production environment and passed config to pool.

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,11 @@
 const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "development";
 
-require("dotenv").config({
-  path: `${__dirname}/../.env.${ENV}`,
-});
+if (ENV !== "production") {
+  require("dotenv").config({
+    path: `${__dirname}/../.env.${ENV}`,
+  });
+}
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
@@ -19,4 +21,4 @@ const config =
       }
     : {};
 
-module.exports = new Pool();
+module.exports = new Pool(config);
