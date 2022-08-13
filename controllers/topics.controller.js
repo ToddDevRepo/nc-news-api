0779bb0c5b80c9eafcdc15482ef23efccc9f471c
ToddDevRepo <todd@LAPTOP-AJE60IS6.localdomain>
2022-08-13 15:03:56 +0100

    Refactor - use await/async in topics request.

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -1,8 +1,7 @@
 const { response } = require("express");
 const { selectAllTopics } = require("../models/topics.model");
 
-module.exports.getTopics = (request, response) => {
-  selectAllTopics().then(({ rows: topics }) => {
-    response.send({ topics });
-  });
+module.exports.getTopics = async (request, response) => {
+  const { rows: topics } = await selectAllTopics();
+  response.send({ topics });
 };
