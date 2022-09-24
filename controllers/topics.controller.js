4dc0b534eef336ba85859408c8a5c8d175fffc92
ToddDevRepo <todd@LAPTOP-AJE60IS6.localdomain>
2022-09-24 15:42:23 +0100

    Refactor - delete unused require

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -1,7 +1,4 @@
-const { response } = require("express");
-const {
-  selectAllTopicsAsync: selectAllTopicsAsync,
-} = require("../models/topics.model");
+const { selectAllTopicsAsync } = require("../models/topics.model");
 
 module.exports.getTopicsAsync = async (request, response) => {
   const topics = await selectAllTopicsAsync();
