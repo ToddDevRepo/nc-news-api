50fcc3eedf9bfff5df4e792cd96c6204becf0563
ToddDevRepo <todd.norton75@gmail.com>
2024-05-31 17:31:56 +0100

    simplify SqlQueriyHelper variable names

--- models/support/sql/core/SqlQueriyHelper.js
+++ models/support/sql/core/SqlQueriyHelper.js
@@ -1,16 +1,16 @@
 class SqlQueryHelper {
-  #_queryable;
+  #_connection;
 
-  constructor(queryable) {
-    this.#_queryable = queryable;
+  constructor(connection) {
+    this.#_connection = connection;
   }
 
   query(sql, params) {
-    return this.#_queryable.query(sql, params);
+    return this.#_connection.query(sql, params);
   }
 
   async queryForRowsAsync(sql, params) {
-    const { rows } = await this.#_queryable.query(sql, params);
+    const { rows } = await this.#_connection.query(sql, params);
     return rows;
   }
 
@@ -20,19 +20,21 @@ class SqlQueryHelper {
   }
 
   async insertColumnValuesAsync(table, columns, values) {
-    const colsStr = columns.join(", ");
-    const valsStr = values.map((val, idx) => `$${idx + 1}`).join(", ");
-    const inserted = await this.queryForItemAsync(
-      `INSERT INTO ${table} (${colsStr}) VALUES (${valsStr}) RETURNING *;`,
+    const columnsString = columns.join(', ');
+    const valueIndicesString = values
+      .map((val, idx) => `$${idx + 1}`)
+      .join(', ');
+    const insertQuery = await this.queryForItemAsync(
+      `INSERT INTO ${table} (${columnsString}) VALUES (${valueIndicesString}) RETURNING *;`,
       values
     );
-    return inserted;
+    return insertQuery;
   }
 
-  async deleteItemWhereAsync(table, colNm, val) {
-    const dbResult = await this.#_queryable.query(
-      `DELETE FROM ${table} WHERE ${colNm} = $1;`,
-      [val]
+  async deleteItemWhereAsync(table, column, value) {
+    const dbResult = await this.#_connection.query(
+      `DELETE FROM ${table} WHERE ${column} = $1;`,
+      [value]
     );
     return dbResult.rowCount !== 0;
   }
@@ -42,10 +44,10 @@ class SqlQueryHelper {
     return rows;
   }
 
-  async selectAllRowsWhereAsync(tableName, colNm, val) {
+  async selectAllRowsWhereAsync(tableName, column, value) {
     const rows = this.queryForRowsAsync(
-      `SELECT * FROM ${tableName} WHERE ${colNm} = $1;`,
-      [val]
+      `SELECT * FROM ${tableName} WHERE ${column} = $1;`,
+      [value]
     );
     return rows;
   }
